48aca4d69729d4896bce1c8f7a6a9cf540bb4a0b
Álvaro Guilherme <107012416+AlvaroGui02@users.noreply.github.com>
2024-03-31 23:21:01 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.backgro}>
+    <><View style={styles.boxlogo}>
+     <Image style={styles.logo} source={require('./assets/logo-pra-fazer.png')}
+     />
     </View>
+
+    <View style={styles.container}>
+      <TextInput
+      style={styles.input}
+       placeholder='E-mail'
+       />
+        
+      <TextInput
+      style={styles.input}
+        placeholder='Senha'
+      />
+
+      <TouchableOpacity style={styles.entre}>
+        <Text style={styles.Text}>Entrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.usu}>Criar Usuário</Text>
+      </TouchableOpacity>
+
+      </View></>
+
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  backgro:{
+    padding: 30,
+    height: '100%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: '#FF6600'
+    },
+  
+    boxlogo:{
+      paddingTop: 300,
+      alignItems: 'center',
+    },
+    container:{
+      padding: 30,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+    },
+    input:{
+      backgroundColor: '#FFF',
+      width:'90%',
+      marginBottom: 15,
+      color: '#222',
+      fontSize: 17,
+      borderRadius: 10,
+      padding: 10,
+    },
+    entre:{
+      backgroundColor: '#070A52',
+      width: '90%',
+      height: 45,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 7,
+    },
+    Text:{
+      color: '#FFF',
+      fontSize: 18,
+      fontWeight: '900',
+    },
+    usu:{
+      color: '#FFF',
+      fontSize: 14,
+      padding: 10,
+    }
 });
+
